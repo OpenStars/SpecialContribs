743b6252da6751dd67ac5b1b558d96e935992dd8
thanhnt <thanhnt@vng.com.vn>
2018-03-08 15:58:05 +0700

    add build_version

---
+++ sources/rocksdb-5.10.4/util/build_version.cc
@@ -0,0 +1,4 @@
+#include "build_version.h"
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:6771e0eb0911d544c257107a69b54d4cd85a2381";
+const char* rocksdb_build_git_date = "rocksdb_build_git_date:2018-03-06";
+const char* rocksdb_build_compile_date = __DATE__;
